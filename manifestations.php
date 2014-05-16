f7965345cd25f956eee88e2eac94efcf81c3d61c
Antoine Lafarge <ant.lafarge@gmail.com>
2014-05-16 12:46:54 +0200

    update manifestations

--- manifestations.php
+++ manifestations.php
@@ -4,32 +4,49 @@
 
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h3 class="panel-title"><strong>Soirée dansante</strong> le <span class="label label-default">vendredi 13 décembre 2013</span> à <span class="label label-default">--h--</span></h3>
+		<h3 class="panel-title"><strong>Stage de danses Folk et Irlandaise</strong> le <span class="label label-default">samedi 7 juin 2013</span> à <span class="label label-default">20h30</span></h3>
 	</div>
 	<div class="panel-body">
-		à la salle de la <span class="label label-primary">Maison pour tous</span> de <span class="label label-primary">Fressenneville</span> <em>(18 rue Charles Vérecque)</em>
+		A la salle de la <span class="label label-primary">Maison pour tous</span> de <span class="label label-primary">Fressenneville</span> <em>(18 rue Charles Vérecque)</em>
 		<br>
-		événement <span class="label label-danger">réservé aux Elèves</span> de l'école uniquement
+		Evénement <span class="label label-danger">réservé aux Elèves</span> de l'école uniquement.
 		<br>
-		sur le principe d'une <span class="label label-info">auberge espagnole</span> donc chacun amène ce qu'il veut <i>(tartes, gateaux, sucreries, boissons...)</i>
+		Sur le principe d'une <span class="label label-info">auberge espagnole</span> donc chacun amène ce qu'il veut <i>(tartes, gateaux, sucreries, boissons...)</i>
 	</div>
 </div>
 
+<!--
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title"><strong>Soirée dansante</strong> le <span class="label label-default">vendredi 13 décembre 2013</span> à <span class="label label-default">--h--</span></h3>
+	</div>
+	<div class="panel-body">
+		A la salle de la <span class="label label-primary">Maison pour tous</span> de <span class="label label-primary">Fressenneville</span> <em>(18 rue Charles Vérecque)</em>
+		<br>
+		Evénement <span class="label label-danger">réservé aux Elèves</span> de l'école uniquement.
+		<br>
+		Sur le principe d'une <span class="label label-info">auberge espagnole</span> donc chacun amène ce qu'il veut <i>(tartes, gateaux, sucreries, boissons...)</i>
+	</div>
+</div>
+-->
+
+<!--
 <div class="panel panel-default">
 	<div class="panel-heading">
 		<h3 class="panel-title"><strong>Fête de fin d'année</strong> le <span class="label label-default">samedi 7 juin 2013</span> à <span class="label label-default">20h30</span></h3>
 	</div>
 	<div class="panel-body">
-		à la salle de la <span class="label label-primary">Maison pour tous</span> de <span class="label label-primary">Fressenneville</span> <em>(18 rue Charles Vérecque)</em>
+		A la salle de la <span class="label label-primary">Maison pour tous</span> de <span class="label label-primary">Fressenneville</span> <em>(18 rue Charles Vérecque)</em>
 		<br>
 		<span class="label label-warning">Sur réservation uniquement</span> auprès de <a href="contacts.php">Véronique Lhermitte</a>
 		<br>
 		<span class="label label-success">Les personnes extérieures à l'école sont les bienvenues</span>
 		<br>
-		un <span class="label label-info">bar</span> avec viennoiseries sera mis à votre disposition, pensez à amener de la monnaie!
+		Un <span class="label label-info">bar</span> avec viennoiseries sera mis à votre disposition, pensez à amener de la monnaie!
 		<br>
 		Danseurs confirmés ou pas, vous êtes les bienvenus!
 	</div>
 </div>
+-->
 
 <?php include_once('php/end.php'); ?>
